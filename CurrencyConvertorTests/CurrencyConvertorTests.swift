b65b3f079a012b98d194b35461ec65f35e37fd92
leticiad7 <leticiaamorimd@icloud.com>
2024-12-05 14:23:23 +0000

    Initial Commit

---
+++ CurrencyConvertorTests/CurrencyConvertorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConvertorTests.swift
+//  CurrencyConvertorTests
+//
+//  Created by Leticia Amorim Domingos on 05/12/2024.
+//
+
+import Testing
+@testable import CurrencyConvertor
+
+struct CurrencyConvertorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
